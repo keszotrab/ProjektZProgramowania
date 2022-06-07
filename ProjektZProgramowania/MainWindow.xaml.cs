84cf1de335aa52c858c85a63eba118859371417b
Bartek <Bartek@BARTEK-KOMPUTER>
2022-06-07 18:44:42 +0200

    Dodaj pliki projektów.

---
+++ ProjektZProgramowania/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjektZProgramowania
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //cartButton.Content =
+            // Assign an image to the button.
+           // button1.Image = Image.FromFile("C:\\Graphics\\MyBitmap.bmp");
+            // Align the image and text on the button.
+          //  button1.ImageAlign = ContentAlignment.MiddleRight;
+           // button1.TextAlign = ContentAlignment.MiddleLeft;
+            // Give the button a flat appearance.
+          //  button1.FlatStyle = FlatStyle.Flat;
+
+        }
+
+        private void Kat1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void cartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Kat2_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Kat3_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
